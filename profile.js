d6b7de7db4a80d5ab2e75356c25bbeef73f7c617
Stavros Klaoudatos <62154974+StavrosKlaoudatos@users.noreply.github.com>
2024-06-04 15:14:31 -0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -20,39 +20,34 @@ const db = getDatabase(app);
 
 document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
-    const researcherId = parseInt(urlParams.get('id'));
+    const researcherId = urlParams.get('id');
 
-    if (isNaN(researcherId)) {
+    if (!researcherId) {
         document.getElementById('profile-container').innerHTML = '<p>Invalid researcher ID.</p>';
         return;
     }
 
-    const researcherRef = ref(db, '/');
+    const researcherRef = ref(db, `/${researcherId}`);
     get(researcherRef)
         .then(snapshot => {
             if (snapshot.exists()) {
-                const researchers = Object.values(snapshot.val());
-                const researcher = researchers[researcherId];
-                if (researcher) {
-                    const profileContainer = document.getElementById('profile-container');
-                    profileContainer.innerHTML = `
-                        <h2 class="text-2xl font-bold mb-2">${researcher.name}</h2>
-                        <p><strong>Position:</strong> ${researcher.position} ${researcher.Emeritus === 1 ? '<span class="bg-gray-200 text-gray-700 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">Emeritus</span>' : ''}</p>
-                        <p><strong>Department:</strong> ${researcher.department}</p>
-                        <p><strong>University:</strong> ${researcher.university}</p>
-                        <p><strong>Email:</strong> <a href="mailto:${researcher.email}" class="text-blue-500">${researcher.email}</a></p>
-                        <p><strong>Interests:</strong> ${researcher.interest}</p>
-                        <p><strong>Research Interests:</strong> ${researcher.research_interests.join(', ')}</p>
-                        <p><strong>Links:</strong> ${researcher.links.map(link => `<a href="${link}" class="text-blue-500">${link}</a>`).join(', ')}</p>
-                        <div id="publications-container">
-                            <h3 class="text-xl font-bold mt-4 mb-2">Recent Publications</h3>
-                            <ul id="publications-list" class="list-disc list-inside"></ul>
-                        </div>
-                    `;
-                    displayPublicationsLink(researcher);
-                } else {
-                    document.getElementById('profile-container').innerHTML = '<p>Researcher not found.</p>';
-                }
+                const researcher = snapshot.val();
+                const profileContainer = document.getElementById('profile-container');
+                profileContainer.innerHTML = `
+                    <h2 class="text-2xl font-bold mb-2">${researcher.name}</h2>
+                    <p><strong>Position:</strong> ${researcher.position} ${researcher.Emeritus === 1 ? '<span class="bg-gray-200 text-gray-700 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">Emeritus</span>' : ''}</p>
+                    <p><strong>Department:</strong> ${researcher.department}</p>
+                    <p><strong>University:</strong> ${researcher.university}</p>
+                    <p><strong>Email:</strong> <a href="mailto:${researcher.email}" class="text-blue-500">${researcher.email}</a></p>
+                    <p><strong>Interests:</strong> ${researcher.interest}</p>
+                    <p><strong>Research Interests:</strong> ${researcher.research_interests.join(', ')}</p>
+                    <p><strong>Links:</strong> ${researcher.links.map(link => `<a href="${link}" class="text-blue-500">${link}</a>`).join(', ')}</p>
+                    <div id="publications-container">
+                        <h3 class="text-xl font-bold mt-4 mb-2">Recent Publications</h3>
+                        <ul id="publications-list" class="list-disc list-inside"></ul>
+                    </div>
+                `;
+                displayPublicationsLink(researcher);
             } else {
                 document.getElementById('profile-container').innerHTML = '<p>Researcher not found.</p>';
             }
@@ -60,11 +55,9 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => console.error('Error fetching data:', error));
 });
 
-
 function displayPublicationsLink(researcher) {
     const query = encodeURIComponent(researcher.name);
     const url = `https://scholar.google.com/scholar?hl=en&q=${query}`;
     const publicationsList = document.getElementById('publications-list');
     publicationsList.innerHTML = `<a href="${url}" class="text-blue-500" target="_blank">View recent publications on Google Scholar</a>`;
 }
-
